b28bae8ad90dc519d0c6a7e1aa99b261abf3ee22
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-09-20 08:19:30 -0300

    Add campos para serem limpos

--- for.js
+++ for.js
@@ -1,3 +1,14 @@
 'use strict'; // Modo restrito
 // Esse modo faz com que o javaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação
 // *Consumos de API - https://viacep.com.br/* 
+
+//Função para limpar o formulário 
+const limparFormulário = () =>{
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('Localidade').value = '';
+    document.getElementById('UF').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('cep').value = '';
+    document.getElementById('complemento').value = '';
+}
