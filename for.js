7c51f88b36eb9ea39a8ea45da9fabef6008664b2
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-09-20 09:27:08 -0300

    Comenta código

--- for.js
+++ for.js
@@ -1,6 +1,6 @@
 'use strict'; // Modo restrito
 // Esse modo faz com que o javaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação
-// *Consumos de API - https://viacep.com.br/* 
+// *Consumos de API - https://viacep.com.br/* -> Site que usamos 
 
 //Função para limpar o formulário 
 const limparFormulário = () =>{
@@ -14,9 +14,12 @@ const limparFormulário = () =>{
 }
 
 // cria expressão regular 
+// const cria uma variavel constante, onde o vaor é fixo  
+// o test faz a verificação se os números digitados estão entre 0 e 9 
 const eNumero = (numero) => /^[0-9]+$/.test(numero); 
 
 // length é uma propriedade que identifica a quantidade de caracteres dentro do argumento cep
+// o operador && retorna true, se estiver com 8 números e dentro de 0 e 9 é true, caso contrário, false 
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
 // Função para preencher formulários com campos da API (interfacede de programção de aplicação)
